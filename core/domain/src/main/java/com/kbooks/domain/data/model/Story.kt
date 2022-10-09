c9cb4e2723b6f12352fc13355cb5f99b4bbc8ff0
hieubui <hieubui00.it@gmail.com>
2022-10-09 11:05:16 +0700

    feat: Setup Story Card component

---
+++ core/domain/src/main/java/com/kbooks/domain/data/model/Story.kt
@@ -0,0 +1,9 @@
+package com.kbooks.domain.data.model
+
+data class Story(
+    val title: String?,
+
+    val author: String?,
+
+    val thumbnail: String?
+)
---
+++ presenters/dashboard/src/main/java/com/kbooks/dashboard/ui/home/component/StoryCard.kt
@@ -0,0 +1,86 @@
+package com.kbooks.dashboard.ui.home.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.kbooks.dashboard.R
+import com.kbooks.domain.data.model.Story
+import com.kma.kbooks.resources.ui.theme.KBooksTheme
+import kotlinx.coroutines.Dispatchers
+
+@Composable
+internal fun StoryCard(
+    modifier: Modifier = Modifier,
+    story: Story
+) {
+    val context = LocalContext.current
+    val model = ImageRequest.Builder(context)
+        .data(story.thumbnail)
+        .dispatcher(Dispatchers.IO)
+        .crossfade(durationMillis = 400)
+        .build()
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(space = 4.dp)
+    ) {
+        AsyncImage(     // Thumbnail
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(ratio = 2f / 3f),
+            model = model,
+            contentScale = ContentScale.FillBounds,
+            contentDescription = stringResource(R.string.thumbnail)
+        )
+
+        Text(   // Title
+            text = story.title.orEmpty(),
+            textAlign = TextAlign.Center,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 2,
+            style = MaterialTheme.typography.body1
+        )
+
+        Text(   // Author
+            text = story.author.orEmpty(),
+            color = Color(0xFF888E96),
+            textAlign = TextAlign.Center,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+            style = MaterialTheme.typography.caption
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun StoryCardPreview() {
+    KBooksTheme {
+        StoryCard(
+            modifier = Modifier.background(color = Color.White),
+            story = Story(
+                title = "Tôi thấy hoa vàng trên cỏ xanh",
+                author = "Nguyễn Nhật Ánh",
+                thumbnail = null
+            )
+        )
+    }
+}
