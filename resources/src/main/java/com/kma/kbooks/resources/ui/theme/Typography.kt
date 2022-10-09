5d04c31851a56a1bea1bfca8ff0e733c782c6f5e
hieubui <hieubui00.it@gmail.com>
2022-10-09 10:59:01 +0700

    feat: Update color, typography for application theme

--- resources/src/main/java/com/kma/kbooks/resources/ui/theme/Typography.kt
+++ resources/src/main/java/com/kma/kbooks/resources/ui/theme/Typography.kt
@@ -1,8 +1,8 @@
 package com.kma.kbooks.resources.ui.theme
 
-import androidx.compose.material.MaterialTheme.colors
 import androidx.compose.material.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
@@ -10,59 +10,38 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.kma.kbooks.resources.R
 
-val typography: Typography
+internal val typography: Typography
     @Composable
     get() = Typography(
         defaultFontFamily = fontFamily,
-        h1 = TextStyle(
-            color = colors.onBackground,
-            fontSize = 48.sp,
-            lineHeight = 12.sp,
-        ),
-        h2 = TextStyle(
-            color = colors.onBackground,
-            fontSize = 32.sp,
-            lineHeight = 8.sp,
-        ),
-        h3 = TextStyle(
-            color = colors.onBackground,
-            fontSize = 28.sp,
-            lineHeight = 7.sp,
-        ),
-        h4 = TextStyle(
-            color = colors.onBackground,
-            fontSize = 22.sp,
-            lineHeight = 5.5.sp,
-        ),
-        h5 = TextStyle(
-            color = colors.onBackground,
-            fontSize = 20.sp,
-            lineHeight = 5.sp,
-        ),
-        h6 = TextStyle(
-            color = colors.onBackground,
-            fontSize = 18.sp,
-            lineHeight = 4.5.sp,
-        ),
-        body1 = TextStyle(
-            color = colors.onBackground,
-            fontSize = 16.sp,
-            lineHeight = 4.sp,
-        ),
-        button = TextStyle(
-            color = colors.onBackground,
-            fontSize = 16.sp,
-            lineHeight = 4.sp,
-        ),
-        caption = TextStyle(
-            color = colors.onBackground,
-            fontSize = 14.sp,
-            lineHeight = 3.sp,
-        ),
+        h1 = textStyle.copy(fontSize = 48.sp),
+        h2 = textStyle.copy(fontSize = 32.sp),
+        h3 = textStyle.copy(fontSize = 28.sp),
+        h4 = textStyle.copy(fontSize = 22.sp),
+        h5 = textStyle.copy(fontSize = 20.sp),
+        h6 = textStyle.copy(fontSize = 18.sp),
+        body1 = textStyle.copy(fontSize = 16.sp),
+        button = textStyle.copy(fontSize = 16.sp),
+        caption = textStyle.copy(fontSize = 14.sp),
     )
 
-val fontFamily = FontFamily(
-    Font(R.font.lato_regular, weight = FontWeight.Normal),
-    Font(R.font.lato_medium, weight = FontWeight.Medium),
-    Font(R.font.lato_bold, weight = FontWeight.Bold)
+private val fontFamily = FontFamily(
+    Font(
+        resId = R.font.lato_regular,
+        weight = FontWeight.Normal
+    ),
+    Font(
+        resId = R.font.lato_medium,
+        weight = FontWeight.Medium
+    ),
+    Font(
+        resId = R.font.lato_bold,
+        weight = FontWeight.Bold
+    )
 )
+
+private val textStyle: TextStyle
+    @Composable
+    get() = TextStyle(
+        color = colorResource(id = R.color.onBackground),
+    )
