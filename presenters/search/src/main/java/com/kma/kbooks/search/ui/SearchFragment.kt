f430ba8ac89e7f925f229d338400cc439b7edb58
hieubui <hieubui00.it@gmail.com>
2022-10-22 09:19:44 +0700

    feat: Initialize Search feature module

---
+++ presenters/search/src/main/java/com/kma/kbooks/search/ui/SearchFragment.kt
@@ -0,0 +1,24 @@
+package com.kma.kbooks.search.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import com.kma.kbooks.resources.ui.theme.KBooksTheme
+
+class SearchFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = ComposeView(requireContext()).apply {
+        setContent {
+            KBooksTheme {
+                TODO("Create UI components for Search screen")
+            }
+        }
+    }
+}
